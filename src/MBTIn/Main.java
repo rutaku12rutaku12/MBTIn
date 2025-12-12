768dbd145fbd3dfeb2984362ab49bea960b28f8a
rutaku12rutaku12 <chookyuho@gmail.com>
2025-12-12 17:03:31 +0900

    first commit

---
+++ src/MBTIn/Main.java
@@ -0,0 +1,12 @@
+package MBTIn;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print(scan.next());
+		System.out.print("whisosiriey");
+		scan.close();
+	}
+}
